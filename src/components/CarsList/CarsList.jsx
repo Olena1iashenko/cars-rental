1553fb59b51d2ce374c9c93e7fdc74cda809eabf
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-06-13 21:50:56 +0300

    set filter by models

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllCarsThunk } from "../../redux/operations";
 import CarItem from "../CarItem/CarItem";
-import s from "./CarsList.module.css";
+import makes from "../../../public/makes.json";
+// import s from "./CarsList.module.css";
 
 const CarsList = () => {
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [selectedMake, setSelectedMake] = useState("");
+  const [filteredCars, setFilteredCars] = useState([]);
 
   const dispatch = useDispatch();
   const cars = useSelector((state) => state.cars.items);
@@ -14,6 +17,19 @@ const CarsList = () => {
   const handleLoadMore = () => {
     setPage((prevPage) => prevPage + 1);
   };
+  useEffect(() => {
+    if (selectedMake === "") {
+      setFilteredCars(cars);
+    } else {
+      const filtered = cars.filter((car) => car.make === selectedMake);
+      setFilteredCars(filtered);
+    }
+  }, [selectedMake, cars]);
+
+  const handleMakeChange = (event) => {
+    setSelectedMake(event.target.value);
+  };
+
   // const hasMoreCars = cars.length % 12 === 0 && cars.length > 0;
   const hasMoreCars = cars.length >= 12 * page;
 
@@ -33,11 +49,27 @@ const CarsList = () => {
 
   return (
     <div>
-      <ul className={s.list}>
+      <div>
+        <select value={selectedMake} onChange={handleMakeChange}>
+          Car brand
+          <option value="Car brand">All Makes</option>
+          {makes.map((make) => (
+            <option key={make} value={make}>
+              {make}
+            </option>
+          ))}
+        </select>
+        <ul>
+          {filteredCars?.map((car) => (
+            <CarItem key={car.id} item={car} />
+          ))}
+        </ul>
+      </div>
+      {/* <ul className={s.list}>
         {cars?.map((item) => (
           <CarItem key={item.id} item={item} />
         ))}
-      </ul>
+      </ul> */}
       {loading ? (
         <p>Loading...</p>
       ) : (
--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,6 +1,27 @@
 import CarsList from "../CarsList/CarsList";
+// import { useState, useEffect } from "react";
+// import { useSelector } from "react-redux";
+// import makes from "../../../public/makes.json";
+// import CarItem from "../CarItem/CarItem";
 
 const Catalog = () => {
+  // const [selectedMake, setSelectedMake] = useState("");
+  // const [filteredCars, setFilteredCars] = useState([]);
+  // const cars = useSelector((state) => state.cars.items);
+
+  // useEffect(() => {
+  //   if (selectedMake === "") {
+  //     setFilteredCars(cars);
+  //   } else {
+  //     const filtered = cars.filter((car) => car.make === selectedMake);
+  //     setFilteredCars(filtered);
+  //   }
+  // }, [selectedMake, cars]);
+
+  // const handleMakeChange = (event) => {
+  //   setSelectedMake(event.target.value);
+  // };
+
   return <CarsList />;
 };
 
