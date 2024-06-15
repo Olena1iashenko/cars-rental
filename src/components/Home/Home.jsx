9da5b5cc07fa36234771ba8fe287b458b1839924
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-06-15 22:54:10 +0300

    small fixes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,11 @@ const Home = () => {
     <div className={s.mainBox}>
       <h2>Welcome to Car Rental</h2>
       <p>Find the perfect car for your next adventure!</p>
-      <img src="../../../public/pngwing.com.png" alt="Car Rental Image" />
+      <img
+        width={600}
+        src="https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg?isig=0&q=80"
+        alt="Car Rental Image"
+      />
     </div>
   );
 };
